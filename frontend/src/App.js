e08359ddd2cf2bd4751b99f6b8e7a098403af7d0
moonheekim0118 <hellomooneekim@gmail.com>
2022-07-31 17:19:58 +0900

    fe 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.ㅇㅇ
+          Edit <code>src/App.js</code> anㅇㅇㅇㅇㅇㅇㅇㅇd save to reload.ㅇㅇ
         </p>
         <a
           className="App-link"
